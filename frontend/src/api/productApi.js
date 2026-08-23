fbf4c83d89213b55f9e125bd7ec12e07e3865396
Paras Joshi <paras521851@gmail.com>
2026-08-23 15:48:50 +0530

    productApi that fetches products from backend

---
+++ frontend/src/api/productApi.js
@@ -0,0 +1,8 @@
+export async function getProducts(){
+    const response = await fetch('http://localhost:3001/products')
+     if (!response.ok) {
+    throw new Error('Failed to fetch products')
+}
+    const data = await response.json()
+    return data
+}
